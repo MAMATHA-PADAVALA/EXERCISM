e13f393d4368d477f5ba70896a8acf800d09a690
MAMATHA-PADAVALA <mamathapadavala146@gmail.com>
2023-12-29 18:17:12 +0530

    nth-prime optimized

--- java/nth-prime/src/main/java/PrimeCalculator.java
+++ java/nth-prime/src/main/java/PrimeCalculator.java
@@ -1,25 +1,33 @@
-class PrimeCalculator {
+public class PrimeCalculator {
 
-    int nth(int nth) {
-    //int final_n=0;
+    int nth(int nth){
+
         if(nth==0)
             throw new IllegalArgumentException();
-        int count=0;
-        int number=2;
-        while(true){
-            int divisorCount=0;
-            for(int i=1;i<=number;i++){
-                if(number%i==0)
-                    divisorCount++;
-            }
-            if(divisorCount==2)
-                count++;
-            if(count==nth)
-                break;
-            number++;
+        int primeCount = 0;
+        int currentNumber = 1;
 
+        while (primeCount < nth) {
+            currentNumber++;
+            if (isPrime(currentNumber)) {
+                primeCount++;
+            }
         }
-        return number;
+
+        return currentNumber;
+    }
+
+    public static boolean isPrime(int num) {
+        int count=0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+        if(count!=2) {
+            return false;
+        }
+        return true;
     }
 
 }
